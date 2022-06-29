bc4ce67d0b95a0a31a97acabf0ac2f005879c1e1
Kiriil-Shark05 <k.gagarin22@gmail.com>
2022-06-29 17:01:57 +0300

    new smooth scroll test

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,21 +1,64 @@
-$(function(){
 
-$('.scroll').on('click', function(e){
-  $('html,body').stop().animate({ scrollTop: $('#preim').offset().top }, 1500);
-  e.preventDefault();
-});
+"use strict"
 
-})
+let header = document.querySelector(".header");
 
-// function ds() {
-//   window.scrollTo({
-//   top: 600,
-//   left: 100,
-//   behavior: 'smooth'
-// }, 1000);
-// }
+let scrollingElement = document.querySelector('[id^="link"]');
+
+if (scrollingElement != null) {
+
+    let scrollingElementId = scrollingElement.getAttribute("id");
+
+    let closestFullscreenOrSwiper;
+
+    if (scrollingElement.closest(".fullscreen")) {
+      scrollingElement.closest(".fullscreen").setAttribute("id", `flag-${scrollingElementId}`);
+      closestFullscreenOrSwiper = scrollingElement.closest(".fullscreen");
+    } else if (scrollingElement.closest(".swiper-container")) {
+      scrollingElement.closest(".swiper-container").setAttribute("id", `flag-${scrollingElementId}`);
+      closestFullscreenOrSwiper = scrollingElement.closest(".swiper-container");
+    }
+
+    let fullscreensCollection = document.querySelectorAll(".fullscreen");
+
+    let swipersCollection = document.querySelectorAll(".swiper-container");
+
+    let fullscreensArray = Array.from(fullscreensCollection);
+
+    let swipersArray = Array.from(swipersCollection);
+
+    let commonArray = fullscreensArray.concat(swipersArray);
+
+    let slicedArray = commonArray.slice(commonArray[0], commonArray.indexOf(closestFullscreenOrSwiper));
+
+    let heigthOfBlocks = [];
+
+    slicedArray.forEach(block => {
+      const blockHeight = block.clientHeight;
+      heigthOfBlocks.push(blockHeight);
+    });
+
+    const initialValue = 0;
+
+    const sumOfBlocks = heigthOfBlocks.reduce(
+      (previosValue, currentValue) => previosValue + currentValue,
+      initialValue
+    );
+
+    const scrollPixels = sumOfBlocks - header.clientHeight;
+
+    console.log(sumOfBlocks);
 
 
+    let anchorLink = document.querySelector('a[href^="#"]');
+
+    anchorLink.addEventListener('click', e => {
+      e.preventDefault();
+      scrollTo({
+          top: scrollPixels,
+          behavior: "smooth"
+      });
+    });
 
 
-// setTimeout(() => {ds()}, 3000);
+}
